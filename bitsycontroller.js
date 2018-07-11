0562158284866ddd9f885b0d5e8f564bee541587
wesley stroupe <wesleystroupe@icloud.com>
2018-07-11 13:49:22 -0400

    moved scripts out of inserted script tags

--- bitsycontroller.js
+++ bitsycontroller.js
@@ -1,6 +1,57 @@
 var controller = document.createElement("div");
 var buttonlogic = document.createElement("script");
 var dynamicbackground = document.createElement("script");
+
+window.fakeKeyPress = (function (direction) {
+  if (!isPlayerEmbeddedInEditor) curPlayerDirection = Direction.None;
+  if (dialogBuffer.IsActive()) {
+    if (dialogBuffer.CanContinue()) {
+      var hasMoreDialog = dialogBuffer.Continue();
+      if (!hasMoreDialog) {
+        console.log("EXIT DIALOG --- onkeydown");
+        onExitDialog();
+      }
+    } else {
+      dialogBuffer.Skip();
+    }
+  } else if (isEnding) {
+    reset_cur_game();
+  } else {
+    curPlayerDirection = direction;
+  }
+  movePlayer(curPlayerDirection);
+  curPlayerDirection = Direction.NONE;
+});
+
+var palWrap = function(f) {
+  var _f = window[f];
+  window[f] = function() {
+    var p1, p2;
+    try {
+      p1 = window.curPal();
+    } catch (e) {
+      p1 = null;
+    }
+    if (_f) {
+      _f.apply(undefined, arguments);
+      p2 = window.curPal();
+      if (p1 !== p2) {
+        changeButtonColors();
+        document.body.style.background =
+          "rgb(" + window.getPal(window.curPal())[0].toString() + ")";
+      }
+    }
+  };
+};
+
+var changeButtonColors = function() {
+  var buttons = document.getElementsByClassName("controller-button");
+  for (i = 0; i < buttons.length; i++) {
+    buttons[i].style.backgroundColor =
+      "rgb(" + window.getPal(window.curPal())[2].toString() + ")";
+  }
+};
+
 document.addEventListener(
   "touchmove",
   function(event) {
@@ -10,6 +61,9 @@ document.addEventListener(
   },
   false
 );
+changeButtonColors();
+document.body.style.background =
+  "rgb(" + window.getPal(window.curPal())[0].toString() + ")";
 
 controller.innerHTML =
   '<div id="controller">' +
@@ -18,7 +72,7 @@ controller.innerHTML =
   "html,body {cursor: pointer; overflow: hidden;}" +
   "canvas { z-index: -10; position: fixed; top: 0vh; height: 100vw; width: 100vw; }" +
   "#controller { position: fixed; top: 60vh; height: 100vw; width: 100vw; left: 0vw; z-index:10; }" +
-  ".controller-button { width: 12.5vw; height: 12.5vw; position:fixed; display:block; border-radius: 100%; border: solid black 1px; background-color: white; overflow: hidden; }" +
+  ".controller-button { width: 12.5vw; height: 12.5vw; position:fixed; display:block; border-radius: 100%; background-color: white; overflow: hidden; }" +
   "#upButton { bottom: 15vh; left:43.75vw; }" +
   "#downButton { bottom: 5vh; left:43.75vw; }" +
   "#leftButton { bottom: 10vh; left: 30vw; }" +
@@ -29,52 +83,10 @@ controller.innerHTML =
   '<button id="rightButton" class="controller-button" onclick="fakeKeyPress(Direction.Right)"></button>' +
   "</div>";
 
-buttonlogic.innerHTML =
-  "function fakeKeyPress(direction) {" +
-  "if (!isPlayerEmbeddedInEditor) curPlayerDirection = Direction.None;" +
-  "if (dialogBuffer.IsActive()) {" +
-  "if (dialogBuffer.CanContinue()) {" +
-  "var hasMoreDialog = dialogBuffer.Continue();" +
-  "if (!hasMoreDialog) {" +
-  "console.log('EXIT DIALOG --- onkeydown');" +
-  "onExitDialog();" +
-  "}" +
-  "} else {" +
-  "dialogBuffer.Skip();" +
-  "}" +
-  "} else if (isEnding) {" +
-  "reset_cur_game();" +
-  "} else {" +
-  "curPlayerDirection = direction;" +
-  "}" +
-  "movePlayer(curPlayerDirection);" +
-  "curPlayerDirection = Direction.NONE;" +
-  "}";
 
-  dynamicbackground.innerHTML =
-  'import bitsy from "bitsy";'+
-  'function palWrap(f) {'+
-  	'var _f = bitsy[f];'+
-  	'bitsy[f] = function () {'+
-  		'var p1, p2;'+
-  		'try {'+
-  			'p1 = bitsy.curPal();'+
-  		'} catch (e) {'+
-  			'p1 = null;'+
-  		'}'+
-  		'if (_f) {'+
-  			'_f.apply(undefined, arguments);'+
-  			'p2 = bitsy.curPal();'+
-  			'if (p1 !== p2) {'+
-  				'document.body.style.background = "rgb(" + bitsy.getPal(bitsy.curPal())[0].toString() + ")";'+
-  			'}'+
-  		'}'+
-  	'};'+
-  '}'+
-  'palWrap("moveSprites");'+
-  'palWrap("movePlayer");'+
-  'palWrap("parseWorld");';
+palWrap("moveSprites");
+palWrap("movePlayer");
+palWrap("parseWorld");
 
 document.body.append(controller);
-document.body.append(buttonlogic);
-document.body.append(dynamicbackground);
+changeButtonColors();
